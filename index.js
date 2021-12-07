e92ecfac6fd7b8febd27ca6b8da8dc51ede773e1
Remon Hany <remonhany@hotmail.com>
2021-12-07 20:26:58 +0200

    046 Environment Variables

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ app.get('/api/courses', (req, res) => {
 })
 
 // Listen on port 3000
-app.listen(3000, () => console.log('Listening on port 3000...'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
