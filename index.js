6a2db7442e419031060d71f292cf041072c3770b
Remon Hany <remonhany@hotmail.com>
2021-12-08 00:47:30 +0200

    051 Input Validation (case 1)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Build a web server
+const Joi = require('joi'); //used for input validation, it returns a class
 const express = require('express');
 const app = express();
 
@@ -20,6 +21,12 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+
+    if(!req.body.name || req.body.name.length < 3) {
+        //400 bad request
+        res.status(400).send('Name is required and should be min 3 characters');
+    }
+
     const course = {
         id: courses.length+1,
         name: req.body.name
