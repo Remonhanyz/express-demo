3135ce7f7f6574ca9ef91467dcdbdacc882ceff8
Remon Hany <remonhany@hotmail.com>
2021-12-08 00:24:55 +0200

    049 Handling HTTP POST Requests

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json()); //important middleware
+
 //create dummy database
 const courses = [
     { id: 1, name: 'course1'},
