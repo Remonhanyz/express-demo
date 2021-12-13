2f63523c26351381a979bfbc1ae7d29e9e8f8048
Remon Hany <remonhany@hotmail.com>
2021-12-14 01:55:30 +0200

    059 Built-In Middleware

--- index.js
+++ index.js
@@ -1,110 +1,110 @@
 // Build a web server
-const Joi = require('joi'); //used for input validation, it returns a class
-const express = require('express');
+const Joi = require("joi"); //used for input validation, it returns a class
+const express = require("express");
 const app = express();
-const log = require('./logger');
+const log = require("./logger");
 
 app.use(express.json()); //important middleware that converts body to JSON
 app.use(log);
+app.use(express.urlencoded({ extended: true })); // do the same thing as express.json but with html forms
+app.use(express.static("public")); //middleware to serve static files (such as css & images ..etc) to the root of the site
 
 //create dummy database
 const courses = [
-    { id: 1, name: 'course1'},
-    { id: 2, name: 'course2'},
-    { id: 3, name: 'course3'},
-]
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-}//this function can be considered as a middleware
-);
+app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
 
-app.post('/api/courses', (req, res) => {
-    const result = validateCourse(req.body);
-    console.log(result);
+app.post("/api/courses", (req, res) => {
+  const result = validateCourse(req.body);
+  console.log(result);
 
-    if(result.error) {
-        //400 bad request
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+  if (result.error) {
+    //400 bad request
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
 
-    const course = {
-        id: courses.length+1,
-        name: req.body.name
-    };
-    //add the new data to database
-    courses.push(course);
-    //return the added data to the user 
-    res.send(course);
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  //add the new data to database
+  courses.push(course);
+  //return the added data to the user
+  res.send(course);
 });
 
 //update a course
-app.put('/api/courses/:id', (req, res) =>{
-    // Look up the course
-    // if doesn't exist return 404
-        //search the array for the requested id
-    let course = courses.find(c => c.id === parseInt(req.params.id));
-        //return a status code 404 if id is not found
-    if (!course) {
-        res.status(404).send('The course with the given ID was not found')
-        return;
-    }
-    
-    //validate
-    // if in valid return 404
-    const result = validateCourse(req.body);
-    if(result.error) {
-        //400 bad request
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+app.put("/api/courses/:id", (req, res) => {
+  // Look up the course
+  // if doesn't exist return 404
+  //search the array for the requested id
+  let course = courses.find((c) => c.id === parseInt(req.params.id));
+  //return a status code 404 if id is not found
+  if (!course) {
+    res.status(404).send("The course with the given ID was not found");
+    return;
+  }
 
-    //update course
-    course.name = req.body.name;
-    //return updated course to client
-    res.send(course);
+  //validate
+  // if in valid return 404
+  const result = validateCourse(req.body);
+  if (result.error) {
+    //400 bad request
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
 
-})
+  //update course
+  course.name = req.body.name;
+  //return updated course to client
+  res.send(course);
+});
 
-app.delete('/api/courses/:id', (req, res) => {
-    // Look up the course
-    // if doesn't exist return 404
-        //search the array for the requested id
-    let course = courses.find(c => c.id === parseInt(req.params.id));
-        //return a status code 404 if id is not found
-    if (!course) {
-        res.status(404).send('The course with the given ID was not found')
-        return;
-    }
-    //delete the course
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
+app.delete("/api/courses/:id", (req, res) => {
+  // Look up the course
+  // if doesn't exist return 404
+  //search the array for the requested id
+  let course = courses.find((c) => c.id === parseInt(req.params.id));
+  //return a status code 404 if id is not found
+  if (!course) {
+    res.status(404).send("The course with the given ID was not found");
+    return;
+  }
+  //delete the course
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
 
-    //return updated course to client
-    res.send(course);
-})
+  //return updated course to client
+  res.send(course);
+});
 
 // search in database
 //eg: localhost:5000/api/courses/1
-app.get('/api/courses/:id', (req, res) => {
-    //search the array for the requested id
-    let course = courses.find(c => c.id === parseInt(req.params.id));
-    //return a status code 404 if id is not found
-    if (!course) {
-        res.status(404).send('The course with the given ID was not found')
-        return;
-    }
-    //return data
-    res.send(course);
-})
+app.get("/api/courses/:id", (req, res) => {
+  //search the array for the requested id
+  let course = courses.find((c) => c.id === parseInt(req.params.id));
+  //return a status code 404 if id is not found
+  if (!course) {
+    res.status(404).send("The course with the given ID was not found");
+    return;
+  }
+  //return data
+  res.send(course);
+});
 
 function validateCourse(course) {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
 
-    return schema.validate(course);
+  return schema.validate(course);
 }
 
 // Listen on port 3000
