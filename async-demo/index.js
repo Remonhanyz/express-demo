eb4721c800e81aac81217dfd5448152318ed9284
Remon Hany <remonhany@hotmail.com>
2021-12-19 09:44:02 +0200

    071 Patterns For Dealing With Asynchronous Code (non-working code)

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,15 @@
 console.log('Before'); //appears 1st (sync)
-setTimeout(()=> {
-    console.log('Reading a user from database....')
-}, 2000); //appears 3rd (Async)
+const user = getUser(1);
+console.log(user)
 console.log('After'); //appears 2nd (sync)
+
+// Callbacks
+// Promises
+// Async/await
+
+function getUser(id) {
+    setTimeout(()=> {
+        console.log('Reading a user from database....')
+        return {id: id, gitHubUsername: 'mosh'}
+    }, 2000);
+}
