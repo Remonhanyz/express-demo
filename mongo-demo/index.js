c1c2e12f1c366a8150cbe3a35b9360f8c251b834
Remon Hany <remonhany@hotmail.com>
2021-12-21 13:43:27 +0200

    086 Schemas

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,3 +4,19 @@ mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err))
 
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now}, //data type is Date and the default value is now (this way you don't have to specify a date when you declare this object)
+    isPublished: Boolean
+})
+
+// Schema Types are:
+    // String
+    // Number
+    // Date
+    // Buffer (for binary values)
+    // Boolean
+    // ObjectID
+    // Array
