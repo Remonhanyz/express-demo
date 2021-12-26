835cf6b40fb26c648c21675854579d445c68ef15
Remon Hany <remonhany@hotmail.com>
2021-12-26 14:51:39 +0200

    089 Querying Documents

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -26,4 +26,13 @@ async function createCourse() {
     console.log(result)
 }
 
+async function getCourses() {
+    const courses = await Course
+        .find({ author: 'Mosh' }) // returns list of documents with the given filters
+        .limit(10) // limit the list to 10 results
+        .sort({ name: 1 }) // sort the list in ascending (1) or descending (-1) order based on a key value
+        .select({ name: 1, tags: 1 }) // select the set of properties that you want to return from the schema (id is always returned  by default)
+    
+    Course.findById() // returns a document
+}
 createCourse()
