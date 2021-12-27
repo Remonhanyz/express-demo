8312e2c14264ba7743852712d8af88e94f3113a1
Remon Hany <remonhany@hotmail.com>
2021-12-27 10:51:36 +0200

    099 Updating a Document- Update First

--- mongo-demo/updating.js
+++ mongo-demo/updating.js
@@ -19,18 +19,25 @@ const courseSchema = mongoose.Schema({
 
 const Course = mongoose.model("Course", courseSchema);
 
+// the previous method returns a whole document which you can then check for a condition in it and then update it
+// here the document is not returned but updated on the database immediatly
+
 async function updateCourses(id) {
-	const course = await Course.findById(id);
-	if (!course) return;
-
-	// course.isPublished = true;
-	// course.author = "Another Author";
-	course.set({
-		isPublished: true,
-		author: "Another Author"
-	});
-
-	const result = await course.save();
+	// this doesn't return the document but updates the document directly on the database returns the number of documents that was modified
+	// const result = await Course.updateOne({ _id: id }, {
+	// 	$set: {
+	// 		author: 'Mosh',
+	// 		isPublished: false
+	// 	}
+	// });
+	
+	// this update the document and returns the updated document
+	const result = await Course.findByIDAndUpdate(id , {
+		$set: {
+			author: 'Mosh',
+			isPublished: false
+		}
+	}, { new: true });
 	console.log(result);
 }
 
