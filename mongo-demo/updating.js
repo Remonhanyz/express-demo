6d2a5e42262cbfe6199aadcdb64c5932a587ee2d
Remon Hany <remonhany@hotmail.com>
2021-12-27 11:15:14 +0200

    102 Validation

--- mongo-demo/updating.js
+++ mongo-demo/updating.js
@@ -6,12 +6,12 @@ mongoose
 	.catch((err) => console.log(err));
 
 const courseSchema = mongoose.Schema({
+	name: { type: String, required: true },
 	tags: [String],
 	date: {
 		type: Date,
 		default: Date.now()
 	},
-	name: String,
 	author: String,
 	isPublished: Boolean,
 	price: Number
@@ -19,8 +19,23 @@ const courseSchema = mongoose.Schema({
 
 const Course = mongoose.model("Course", courseSchema);
 
-// the previous method returns a whole document which you can then check for a condition in it and then update it
-// here the document is not returned but updated on the database immediatly
+async function createCourse() {
+	const course = new Course({
+		name: "Angular Course",
+		author: "Mosh",
+		tags: ["angular", "backend"],
+		isPublished: true,
+		price: 15
+	});
+	try {
+		// await course.validate(); //checks if the given course (document) data is valid
+		const result = await course.save();
+		console.log(result);
+	}
+	catch (ex) {
+		console.log(ex.message)
+	}
+}
 
 async function updateCourses(id) {
 	const result = await Course.findByIDAndUpdate(id , {
@@ -34,9 +49,8 @@ async function updateCourses(id) {
 
 
 async function removeCourses(id) {
-	// const result = await Course.deleteOne({ _id: id }); // (use deleteMany for deleting multiple documents) return the number of deleted documents
 	const result = await Course.findByIdAndRemove(id); //returns the document then delete it
 	console.log(result);
 }
 
-removeCourses("5a68fdc3615eda645bc6bdec");
+// removeCourses("5a68fdc3615eda645bc6bdec");
