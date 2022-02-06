0ae64765b245cfcd2c3d7b63776570d5cf16f5f4
Remon Hany <remonhany2000@hotmail.com>
2022-02-06 22:59:13 +0200

    104 Custom Validators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,39 +1,73 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/playground')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err))
+mongoose
+	.connect("mongodb://localhost/playground")
+	.then(() => console.log("Connected to MongoDB..."))
+	.catch((err) => console.error("Could not connect to MongoDB...", err));
 
 const courseSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [String],
-    date: {type: Date, default: Date.now}, //data type is Date and the default value is now (this way you don't have to specify a date when you declare this object)
-    isPublished: Boolean
-})
+    name: {
+        type: String,
+        required: true,
+        minlength: 5, maxlength: 255,
+        // match: /pattern/
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['web', 'mobile', 'network'] // array of valid inputs
+    },
+	author: String,
+    tags: {
+        type: Array,
+        validate: {
+            validator: function (v) {
+                return v && v.length > 0
+            }, //the array must not be empty
+            message: "A course should have at least one tag"
+        }
+    },
+	date: {type: Date, default: Date.now}, //data type is Date and the default value is now (this way you don't have to specify a date when you declare this object)
+	isPublished: Boolean,
+	price: {
+		type: Number,
+		required: function () {
+			return this.isPublished;
+        },
+        min: 10,
+        max: 200
+	}
+});
 
-const Course = mongoose.model('Course', courseSchema) // a Course class
+const Course = mongoose.model("Course", courseSchema); // a Course class
 
 async function createCourse() {
-    const course = new Course({
-        name: 'Node.js Course',
-        author: 'Mosh',
-        tags: ['node', 'backend'],
-        isPublished: true
-    }) // an object of the class Course
+	const course = new Course({
+		name: "Node.js Course",
+		author: "Mosh",
+		tags: ["node", "backend"],
+		isPublished: true
+	}); // an object of the class Course
 
-    const result = await course.save() //return the course object that is saved in database (id is added automatically)
-    console.log(result)
+	try {
+		// await course.validate((err) => {
+		//     if (err) {
+
+		// 	}
+		// }); // check if there is any error and if so does a function
+		const result = await course.save(); //return the course object that is saved in database (id is added automatically)
+		console.log(result);
+	} catch (exeption) {
+		console.log(exeption.message);
+	}
 }
 
 async function getCourses(pageNumber, pageSize) {
-    // const pageNumber = 2
-    const courses = await Course
-        .find({ author: /.*Mosh.*/i }) // regular expressions
-        .skip((pageNumber - 1) * pageSize)
-        .limit(pageSize) // limit the list to 10 results
-        .sort({ name: 1 }) // sort the list in ascending (1) or descending (-1) order based on a key value
-        .select({ name: 1, tags: 1 }) // select the set of properties that you want to return from the schema (id is always returned  by default)
-    
+	// const pageNumber = 2
+	const courses = await Course.find({author: /.*Mosh.*/i}) // regular expressions
+		.skip((pageNumber - 1) * pageSize)
+		.limit(pageSize) // limit the list to 10 results
+		.sort({name: 1}) // sort the list in ascending (1) or descending (-1) order based on a key value
+		.select({name: 1, tags: 1}); // select the set of properties that you want to return from the schema (id is always returned  by default)
 }
-getCourses(2, 10)
+getCourses(2, 10);
