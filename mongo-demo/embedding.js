6539556dcb40bbfb86d74558c22a51222e80bf6f
Remon Hany <remonhany2000@hotmail.com>
2022-02-10 15:06:56 +0200

    116 Using an Array of Sub-documents

--- mongo-demo/embedding.js
+++ mongo-demo/embedding.js
@@ -14,13 +14,13 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  author: {
-    type: authorSchema,
+  authors: {
+    type: [authorSchema],
     required: true
   }
 }));
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
   const course = new Course({
     name, 
     author
@@ -33,6 +33,19 @@ async function createCourse(name, author) {
 async function listCourses() { 
   const courses = await Course.find();
   console.log(courses);
+}
+
+async function addAuthor(courseId, author) {
+	const course = await Course.findById(courseId);
+	course.author.push(author);
+	course.save();
+}
+
+async function removeAuthor(courseId, authorId) {
+	const course = await Course.findById(courseId);
+	const author = course.authors.id(authorId);
+  author.remove();
+  course.save()
 }
 
 async function updateAuthor(courseId) {
@@ -49,4 +62,7 @@ async function updateAuthor(courseId) {
   // course.save()
 }
 
-createCourse('Node Course', new Author({ name: 'Mosh' }));
+createCourse("Node Course", [
+	new Author({name: "Mosh"}),
+	new Author({name: "Mosh"})
+]);
